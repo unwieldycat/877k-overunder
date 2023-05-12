d55d7da2b7b07c6c46a6d85a9f3c4803a030136b
Thurston Yates <thurstonalexyates@gmail.com>
2023-05-12 15:44:25 -0400

    Disable simple names

--- include/main.h
+++ include/main.h
@@ -24,7 +24,7 @@
  * E_CONTROLLER_MASTER is pedantically correct within the PROS styleguide, but
  * not convienent for most student programmers.
  */
-#define PROS_USE_SIMPLE_NAMES
+// #define PROS_USE_SIMPLE_NAMES
 
 /**
  * If defined, C++ literals will be available for use. All literals are in the
@@ -39,7 +39,6 @@
 /**
  * You should add more #includes here
  */
-//#include "okapi/api.hpp"
 
 /**
  * If you find doing pros::Motor() to be tedious and you'd prefer just to do
@@ -74,7 +73,7 @@ void opcontrol(void);
 /**
  * You can add C++-only headers here
  */
-//#include <iostream>
+// #include <iostream>
 #endif
 
-#endif  // _PROS_MAIN_H_
+#endif // _PROS_MAIN_H_
