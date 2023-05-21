dbd23572ae4f9436659308378616b7e92cabebc4
Thurston Yates <thurstonalexyates@gmail.com>
2023-05-20 23:56:59 -0400

    Basic chassis movement function

---
+++ include/chassis.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include "main.h"
+#include "pid.hpp"
+
+namespace chassis {
+
+void drive(int distance);
+
+}
---
+++ src/chassis.cpp
@@ -0,0 +1,23 @@
+#include "chassis.hpp"
+#include "devices.hpp"
+#include "pid.hpp"
+
+PIDController drive_pid(1, 1, 1);
+
+void chassis::drive(int distance) {
+	drive_pid.reset();
+	drive_pid.set_goal(distance, 0.5);
+
+	float current_pos;
+	float output;
+
+	while (!drive_pid.goal_met()) {
+		current_pos = 0; // TODO: Odometry
+		output = drive_pid.calculate(current_pos);
+		drive_left.move(output);
+		drive_right.move(-output);
+	}
+
+	drive_left.brake();
+	drive_right.brake();
+}
