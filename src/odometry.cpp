68426730a2c89ccb097b59826a299d62e5c116fd
Will Carter <whcarter212@gmail.com>
2023-05-22 16:30:17 -0400

    Clean up odometry to match code standards

--- src/odometry.cpp
+++ src/odometry.cpp
@@ -4,8 +4,8 @@
 //Disregard for now, massive mess - will clean up
 class Odometry {
 private: 
-	int odom_x = 0;
-	int odom_y = 0;
+	float odom_x = 0;
+	float odom_y = 0;
 	const float LEFT_OFFSET = 0;
 	const float RIGHT_OFFSET = 0;
 	const float REAR_OFFSET = 0;
@@ -13,11 +13,6 @@ private:
 	float right_odom_dist = 0;
 	float rear_odom_dist = 0;
 	float imu_heading = 0;
-	float previous_heading = 0;
-	float trackingX = 0;
-	float trackingY = 0;
-	float odomX = 0;
-	float odomY = 0;
 
 public:
 	Odometry() {
@@ -31,81 +26,81 @@ public:
 	}
 
 	//Converts robot-centric coordinates to field-centric
-	float localToGlobalCoords(float localX, float localY, float robotHeading, bool returnX = true) {
-		float headingTraveled = robotHeading;
-		float distanceTraveled = sqrt(pow(localX, 2) + pow(localY, 2));
-		//printf("%f\n", distanceTraveled);
-		if (localX >= 0 && localY > 0) {
-			headingTraveled = (M_PI/2) - (robotHeading + (M_PI/2) - fabs(atan(localY / localX)));
-		} else if (localX > 0 && localY <= 0) {
-			headingTraveled = (M_PI/2) - (robotHeading + (M_PI/2) + fabs(atan(localY / localX)));
-		} else if (localX <= 0 && localY < 0) {
-			headingTraveled = (M_PI/2) - (robotHeading + (3*M_PI/2) - fabs(atan(localY / localX)));
-		} else if (localX < 0 && localY >= 0) {
-			headingTraveled = (M_PI/2) - (robotHeading + (3*M_PI/2) + fabs(atan(localY / localX)));
+	float localToGlobalCoords(float local_x, float local_y, float robot_heading, bool return_x = true) {
+		float heading_traveled = robot_heading;
+		float distance_traveled = sqrt(pow(local_x, 2) + pow(local_y, 2));
+
+		if (local_x >= 0 && local_y > 0) {
+			heading_traveled = (M_PI/2) - (robot_heading + (M_PI/2) - fabs(atan(local_y / local_x)));
+		} else if (local_x > 0 && local_y <= 0) {
+			heading_traveled = (M_PI/2) - (robot_heading + (M_PI/2) + fabs(atan(local_y / local_x)));
+		} else if (local_x <= 0 && local_y < 0) {
+			heading_traveled = (M_PI/2) - (robot_heading + (3*M_PI/2) - fabs(atan(local_y / local_x)));
+		} else if (local_x < 0 && local_y >= 0) {
+			heading_traveled = (M_PI/2) - (robot_heading + (3*M_PI/2) + fabs(atan(local_y / local_x)));
 		} 
-		while (headingTraveled >= 2*M_PI) {
-			headingTraveled -= 2*M_PI;
+		while (heading_traveled >= 2*M_PI) {
+			heading_traveled -= 2*M_PI;
 		} 
-		while (headingTraveled < 0) {
-			headingTraveled += 2*M_PI;
+		while (heading_traveled < 0) {
+			heading_traveled += 2*M_PI;
 		}
 
-		float globalX = distanceTraveled * cos(headingTraveled);
-		float globalY = distanceTraveled * sin(headingTraveled);
+		float global_x = distance_traveled * cos(heading_traveled);
+		float global_y = distance_traveled * sin(heading_traveled);
 
-		//printf("X position: %f\nY position: %f\n", globalX, globalY);
-
-		if (returnX) {
-			return globalX;
+		if (return_x) {
+			return global_x;
 		} else {
-			return globalY;
+			return global_y;
 		}
 	}
 
 	//Converts field-centric coordinates to robot-centric
-	float globalToLocalCoords(float globalX, float globalY, float robotX, float robotY, float robotHeading, bool returnX = true) {
-		float globalXDist = globalX - robotX;
-		float globalYDist = globalY - robotY;
-		float straightDist = sqrt(pow(globalXDist, 2) + pow(globalYDist, 2));
-		float angleToTarget = 0;
+	float globalToLocalCoords(float global_x, float global_y, float robot_x, float robot_y, float robot_heading, bool return_x = true) {
+		float global_x_dist = global_x - robot_x;
+		float global_y_dist = global_y - robot_y;
+		float straight_dist = sqrt(pow(global_x_dist, 2) + pow(global_y_dist, 2));
+		float angle_to_target = 0;
 
-		if (globalXDist >= 0 && globalYDist > 0) {          //Quadrant 1
-			angleToTarget = fabs(atan(globalYDist / globalXDist)) + robotHeading;
-		} else if (globalXDist > 0 && globalYDist <= 0) {   //Quadrant 4
-			//printf("%f\n", fabs(atan(globalYDist / globalXDist)));
-			angleToTarget = 2*M_PI - fabs(atan(globalYDist / globalXDist)) + robotHeading;
-		} else if (globalXDist <= 0 && globalYDist < 0) {   //Quadrant 3
-			angleToTarget = M_PI + fabs(atan(globalYDist / globalXDist)) - robotHeading;
-		} else if (globalXDist < 0 && globalYDist >= 0) {   //Quadrant 2
-			angleToTarget = M_PI - fabs(atan(globalYDist / globalXDist)) + robotHeading;
+		if (global_x_dist >= 0 && global_y_dist > 0) {          //Quadrant 1
+			angle_to_target = fabs(atan(global_y_dist / global_x_dist)) + robot_heading;
+		} else if (global_x_dist > 0 && global_y_dist <= 0) {   //Quadrant 4
+			angle_to_target = 2*M_PI - fabs(atan(global_y_dist / global_x_dist)) + robot_heading;
+		} else if (global_x_dist <= 0 && global_y_dist < 0) {   //Quadrant 3
+			angle_to_target = M_PI + fabs(atan(global_y_dist / global_x_dist)) - robot_heading;
+		} else if (global_x_dist < 0 && global_y_dist >= 0) {   //Quadrant 2
+			angle_to_target = M_PI - fabs(atan(global_y_dist / global_x_dist)) + robot_heading;
 		} 
 
-		float localX = straightDist * cos(angleToTarget);
-		float localY = straightDist * sin(angleToTarget);
-		//printf("%f\n", angleToTarget);
+		float local_x = straight_dist * cos(angle_to_target);
+		float local_y = straight_dist * sin(angle_to_target);
 
-		//printf("localX: %f\nlocalY: %f\n", localX, localY);
-
-		if (returnX) {
-			return localX;
+		if (return_x) {
+			return local_x;
 		} else {
-			return localY;
+			return local_y;
 		}
 	}
 
 	void track_position() {
+		float previous_heading = 0;
+		float theta = 0;
+		float tracking_x = 0;
+		float tracking_y = 0;
+		float localXDist = 0;
+		float localYDist = 0;
+
 		while (true) {
 			left_odom_dist = odom_left.get_position()*100/360 * (2.75 * M_PI);   //distance traveled by left tracking wheel since last poll
 			right_odom_dist = odom_left.get_position()*100/360 * (2.75 * M_PI);
 			rear_odom_dist = odom_left.get_position()*100/360 * (2.75 * M_PI);
 
 			imu_heading = imu.get_heading();
-			float theta = imu_heading - previous_heading;
-			//printf("IMU Heading: %f\n", imu_heading);
+			theta = imu_heading - previous_heading;
 
-			float localXDist = 0;
-			float localYDist = 0;
+			localXDist = 0;
+			localYDist = 0;
 			if (theta != 0) {
 			localXDist = 2 * (rear_odom_dist / theta + REAR_OFFSET) * (sin(theta / 2));
 			localYDist = (left_odom_dist + right_odom_dist) / theta * (sin(theta / 2));
@@ -114,13 +109,11 @@ public:
 			localYDist = (right_odom_dist + left_odom_dist) / 2;
 			}
 
-			trackingX += localToGlobalCoords(localXDist, localYDist, imu_heading, true);
-			trackingY += localToGlobalCoords(localXDist, localYDist, imu_heading, false);
+			tracking_x += localToGlobalCoords(localXDist, localYDist, imu_heading, true);
+			tracking_y += localToGlobalCoords(localXDist, localYDist, imu_heading, false);
 
-			odomX = trackingX + localToGlobalCoords(RIGHT_OFFSET - LEFT_OFFSET, REAR_OFFSET, imu_heading, true);
-			odomY = trackingY + localToGlobalCoords(RIGHT_OFFSET - LEFT_OFFSET, REAR_OFFSET, imu_heading, false);
-
-			//printf("X position: %f\nY position: %f\n", odomX, odomY);
+			odom_x = tracking_x + localToGlobalCoords(RIGHT_OFFSET - LEFT_OFFSET, REAR_OFFSET, imu_heading, true);
+			odom_y = tracking_y + localToGlobalCoords(RIGHT_OFFSET - LEFT_OFFSET, REAR_OFFSET, imu_heading, false);
 
 			odom_left.reset_position();
 			odom_right.reset_position();
@@ -131,10 +124,10 @@ public:
 		}
 	}
 
-	int get_x() {
-		return odomX;
+	float get_x() {
+		return odom_x;
 	}
-	int get_y() {
-		return odomY;
+	float get_y() {
+		return odom_y;
 	}
 };
