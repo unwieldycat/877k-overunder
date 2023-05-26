328091c36fb77bde44c52f038c7962c768817d30
Thurston Yates <thurstonalexyates@gmail.com>
2023-05-26 19:36:22 -0400

    Add turning PID

--- src/chassis.cpp
+++ src/chassis.cpp
@@ -4,19 +4,20 @@
 #include "pid.hpp"
 
 PIDController drive_pid(1, 1, 1);
+PIDController turn_pid(1, 1, 1);
 
 void chassis::drive(int distance) {
-	drive_pid.reset();
 	drive_pid.set_goal(distance, 0.5);
+	turn_pid.set_goal(0, 0.5);
 
-	float current_pos;
-	float output;
+	float drive;
+	float turn;
 
 	while (!drive_pid.goal_met()) {
-		current_pos = odom::get_x();
-		output = drive_pid.calculate(current_pos);
-		drive_left.move(output);
-		drive_right.move(-output);
+		drive = drive_pid.calculate(odom::get_x());
+		turn = turn_pid.calculate(odom::get_y());
+		drive_left.move(drive - turn);
+		drive_right.move(-(drive + turn));
 	}
 
 	drive_left.brake();
