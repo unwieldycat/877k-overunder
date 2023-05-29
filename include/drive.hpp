aca8c2d90e951eb35d453a40acf9562f8eec30d3
Will Carter <whcarter212@gmail.com>
2023-05-29 16:12:37 -0400

    Add drive and getter functions

--- include/drive.hpp
+++ include/drive.hpp
@@ -2,8 +2,12 @@
 #include "main.h"
 
 namespace drive {
-	void select_drive_mode(std::string drive_mode);
 	void tank_drive();
 	void arcade_drive();
 	void curvature_drive();
+	void drive();
+	void select_drive_mode(int drive_mode);
+	void select_drive_mode(std::string drive_mode);
+	int get_drive_mode();
+	std::string get_drive_name();
 }
