39f4b8b4608eca79efa08b22aae5481bf4fc31f3
Thurston Yates <thurstonalexyates@gmail.com>
2023-06-01 08:45:09 -0400

    Add documentation

--- include/chassis.hpp
+++ include/chassis.hpp
@@ -4,10 +4,19 @@
 
 namespace chassis {
 
+/**
+ * Drive a distance (feet)
+ */
 void drive(double distance);
 
+/**
+ * Turn to a specific heading
+ */
 void turn_abs(double heading);
 
+/**
+ * Turn relative to the robot's current position
+ */
 void turn_rel(double degrees);
 
 } // namespace chassis
