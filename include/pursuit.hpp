7c79f41ab5845579533025c19ea6d65b518ade5d
Kit <yuhanli2006@gmail.com>
2023-09-03 19:33:29 -0400

    Extern coordinate vectors

--- include/pursuit.hpp
+++ include/pursuit.hpp
@@ -2,12 +2,15 @@
 #include "devices.hpp"
 #include "main.h"
 
+extern std::vector<double> points_x;
+extern std::vector<double> points_y;
+
 namespace pursuit {
 
 void add_point(double x_ft, double y_ft);
 
 void pursuit(
-    int lookahead_distance, int voltage_constant, int lowest_x, int lowest_y, int highest_x,
+    double lookahead_distance, int voltage_constant, int lowest_x, int lowest_y, int highest_x,
     int highest_y
 );
 
