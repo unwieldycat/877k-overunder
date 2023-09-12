8404d866ccbb6a4e2ae3ac1fb59e72cc1122bd56
Kit <yuhanli2006@gmail.com>
2023-09-11 20:58:44 -0400

    Move comments to where they're supposed to go

--- include/pursuit.hpp
+++ include/pursuit.hpp
@@ -2,12 +2,34 @@
 #include "devices.hpp"
 #include "main.h"
 
+/**
+ * @brief Create vector of pairs to store points for the path with initial point 0, 0
+ *
+ */
 extern std::vector<std::pair<foot_t, foot_t>> points;
 
 namespace pursuit {
 
+/**
+ * @brief adds a point to the end of the points for the path
+ *
+ * @param x_ft x coordinate in feet
+ * @param y_ft y coordinate in feet
+ */
 void add_point(foot_t x_ft, foot_t y_ft);
 
+/**
+ * @brief Begins moving the robot following all the points stored previous to the running of this
+ * function, clears the points after running
+ *
+ * @param lookahead_Distance How far away the point the robot is "Chasing" is
+ * @param voltage_constant How fast the robot will be moving forward if it is traveling on a
+ * straight path
+ * @param lowest_x Bottom left x coordinate in feet of the restricted area
+ * @param lowest_y Bottom left y coordinate in feet of the restricted area
+ * @param highest_x Bottom right x coordinate in feet of the restricted area
+ * @param highest_y Bottom right y coordinate in feet of the restricted area
+ */
 void pursuit(
     foot_t lookahead_distance, int voltage_constant, foot_t lowest_x, foot_t lowest_y,
     foot_t highest_x, foot_t highest_y
--- src/pursuit.cpp
+++ src/pursuit.cpp
@@ -5,36 +5,14 @@
 #include "units.h"
 using namespace units::math;
 
-/**
- * @brief Create vector of pairs to store points for the path with initial point 0, 0
- *
- */
 std::vector<std::pair<foot_t, foot_t>> points = {std::make_pair(0_ft, 0_ft)};
 
-/**
- * @brief adds a point to the end of the points for the path
- *
- * @param x_ft x coordinate in feet
- * @param y_ft y coordinate in feet
- */
 void pursuit::add_point(foot_t x_ft, foot_t y_ft) {
 	if (points[points.size() - 1].first != x_ft || points[points.size() - 1].second != y_ft) {
 		points.push_back({x_ft, y_ft});
 	}
 }
 
-/**
- * @brief Begins moving the robot following all the points stored previous to the running of this
- * function, clears the points after running
- *
- * @param lookahead_Distance How far away the point the robot is "Chasing" is
- * @param voltage_constant How fast the robot will be moving forward if it is traveling on a
- * straight path
- * @param lowest_x Bottom left x coordinate in feet of the restricted area
- * @param lowest_y Bottom left y coordinate in feet of the restricted area
- * @param highest_x Bottom right x coordinate in feet of the restricted area
- * @param highest_y Bottom right y coordinate in feet of the restricted area
- */
 void pursuit::pursuit(
     foot_t lookahead_Distance, int voltage_constant, foot_t lowest_x = 0_ft, foot_t lowest_y = 0_ft,
     foot_t highest_x = 0_ft, foot_t highest_y = 0_ft
