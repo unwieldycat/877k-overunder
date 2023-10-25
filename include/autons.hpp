73e17f3b90fafc9e7706be6c5edafe53920859e0
Thurston Yates <thurstonalexyates@gmail.com>
2023-10-25 07:46:31 -0400

    Create simple autons

---
+++ include/autons.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include "main.h"
+
+/**
+ * Park robot next to left goal, starting from left side
+ */
+void park_left();
+
+/**
+ * Park robot next to right goal, starting from right side
+ */
+void park_right();
