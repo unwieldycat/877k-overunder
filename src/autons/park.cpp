2737862202447a6d3fa3b4bf0146862767ccf593
Thurston Yates <thurstonalexyates@gmail.com>
2023-10-27 09:46:05 -0400

    Install and configure robodash & liblvgl

--- src/autons/park.cpp
+++ src/autons/park.cpp
@@ -1,15 +1,16 @@
 #include "autons.hpp"
 #include "chassis.hpp"
 
-void park_out(bool left) {
-	chassis::turn_rel(-45_deg);
-	chassis::drive(5.65_ft);
-	chassis::turn_abs(90_deg);
-}
-
-void park_in(bool left) {
+void park_right() {
 	chassis::drive(2_ft);
 	chassis::turn_rel(-90_deg);
 	chassis::drive(2_ft);
 	chassis::turn_abs(90_deg);
+}
+
+void park_left() {
+	chassis::drive(2_ft);
+	chassis::turn_rel(90_deg);
+	chassis::drive(2_ft);
+	chassis::turn_abs(-90_deg);
 }
