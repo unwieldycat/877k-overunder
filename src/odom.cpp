f4529a9a14789747ac5a570883c35fc10ecc8ad9
Kit <yuhanli2006@gmail.com>
2023-11-25 13:47:57 -0500

    Odom partially functional (forward and backward)

--- src/odom.cpp
+++ src/odom.cpp
@@ -2,8 +2,8 @@
 #include "devices.hpp"
 #include "units.h"
 
-const std::pair<inch_t, inch_t> left_offset = {-3.354_in, 4.244_in};
-const std::pair<inch_t, inch_t> rear_offset = {0_in, -5.571_in};
+const std::pair<inch_t, inch_t> left_offset = {6_in, -2.25_in};
+const std::pair<inch_t, inch_t> rear_offset = {0_in, -2.5_in};
 
 using namespace units::math;
 
@@ -16,8 +16,8 @@ degree_t imu_heading = 0_deg;
 void odom::initialize() {
 	odom_left.reset_position();
 	odom_rear.reset_position();
-	odom_left.set_reversed(true);
-	odom_rear.set_reversed(true);
+	odom_left.set_reversed(false);
+	odom_rear.set_reversed(false);
 	imu.reset(true);
 
 	while (std::isinf(imu.get_heading())) {
@@ -25,116 +25,57 @@ void odom::initialize() {
 	}
 }
 
-// Converts robot-centric coordinates to field-centric
-std::pair<inch_t, inch_t>
-odom::local_to_global_coords(inch_t local_x, inch_t local_y, degree_t robot_heading) {
-	degree_t heading_traveled = robot_heading;
-	inch_t distance_traveled = sqrt(pow<2>(local_x) + pow<2>(local_y));
-
-	if (local_x >= 0_in && local_y > 0_in) {
-		heading_traveled = degree_t(M_PI / 2) -
-		                   (robot_heading + degree_t(M_PI / 2) - fabs(atan(local_y / local_x)));
-	} else if (local_x > 0_in && local_y <= 0_in) {
-		heading_traveled = degree_t(M_PI / 2) -
-		                   (robot_heading + degree_t(M_PI / 2) + fabs(atan(local_y / local_x)));
-	} else if (local_x <= 0_in && local_y < 0_in) {
-		heading_traveled = degree_t(M_PI / 2) -
-		                   (robot_heading + degree_t(3 * M_PI / 2) - fabs(atan(local_y / local_x)));
-	} else if (local_x < 0_in && local_y >= 0_in) {
-		heading_traveled = degree_t(M_PI / 2) -
-		                   (robot_heading + degree_t(3 * M_PI / 2) + fabs(atan(local_y / local_x)));
-	}
-
-	while (heading_traveled >= 2_deg * M_PI) {
-		heading_traveled -= 2_deg * M_PI;
-	}
-
-	while (heading_traveled < 0_deg) {
-		heading_traveled += 2_deg * M_PI;
-	}
-
-	inch_t global_x = distance_traveled * cos(heading_traveled);
-	inch_t global_y = distance_traveled * sin(heading_traveled);
-
-	return {global_x, global_y};
-}
-
-// Converts field-centric coordinates to robot-centric
-std::pair<inch_t, inch_t> odom::global_to_local_coords(
-    inch_t global_x, inch_t global_y, inch_t robot_x, inch_t robot_y, degree_t robot_heading
-) {
-	inch_t global_x_dist = global_x - robot_x;
-	inch_t global_y_dist = global_y - robot_y;
-	inch_t straight_dist = sqrt(pow<2>(global_x_dist) + pow<2>(global_y_dist));
-	degree_t angle_to_target = 0_deg;
-
-	if (global_x_dist >= 0_in && global_y_dist > 0_in) { // Quadrant 1
-		angle_to_target = fabs(atan(global_y_dist / global_x_dist)) + robot_heading;
-	} else if (global_x_dist > 0_in && global_y_dist <= 0_in) { // Quadrant 4
-		angle_to_target =
-		    degree_t(2 * M_PI) - fabs(atan(global_y_dist / global_x_dist)) + robot_heading;
-	} else if (global_x_dist <= 0_in && global_y_dist < 0_in) { // Quadrant 3
-		angle_to_target =
-		    degree_t(M_PI) + fabs(atan(global_y_dist / global_x_dist)) - robot_heading;
-	} else if (global_x_dist < 0_in && global_y_dist >= 0_in) { // Quadrant 2
-		angle_to_target =
-		    degree_t(M_PI) - fabs(atan(global_y_dist / global_x_dist)) + robot_heading;
-	}
-
-	inch_t local_x = straight_dist * cos(angle_to_target);
-	inch_t local_y = straight_dist * sin(angle_to_target);
-
-	return {local_x, local_y};
-}
-
 [[noreturn]] void odom::track_position() {
-	degree_t previous_heading = degree_t(imu.get_heading());
-	degree_t theta;
-	inch_t tracking_x = odom_x;
-	inch_t tracking_y = odom_y;
-	inch_t local_dist_x;
-	inch_t local_dist_y;
-	inch_t x_radius;
-	inch_t y_radius;
+	auto left_dist = 0_rad * 0_in, rear_dist = 0_rad * 0_in;
+	inch_t y_radius, x_radius, local_x, local_y, total_dist;
+	degree_t prev_heading = (degree_t)(imu.get_heading()), heading_change, robot_heading, 
+	local_true_heading, global_true_heading, left_odom, rear_odom;
+	while(true){
+		left_odom = (degree_t)(odom_left.get_position() / 100.0);
+		rear_odom = (degree_t)(odom_rear.get_position() / 100.0);
+		robot_heading = (degree_t)(imu.get_heading());
+		left_dist = (radian_t)(left_odom) * 2.75_in / 2;
+		rear_dist = (radian_t)(rear_odom) * 2.75_in / 2;
+		heading_change = robot_heading - prev_heading;
 
-	while (true) {
-		// distance traveled by left tracking wheel since last poll
-		left_odom_dist = inch_t((odom_left.get_position() / 100.0) / 360.0 * (2.75 * M_PI));
-		rear_odom_dist = inch_t((odom_rear.get_position() / 100.0) / 360.0 * (2.75 * M_PI));
+		//TODO: prevent heading change from being 359 in turns
 
-		imu_heading = degree_t(imu.get_heading());
-		theta = imu_heading - previous_heading;
-
-		local_dist_x = 0_in;
-		local_dist_y = 0_in;
-
-		if (theta != 0_deg) {
-			x_radius = inch_t(unit_cast<double>(rear_odom_dist / theta)) + rear_offset.second;
-			y_radius = inch_t(unit_cast<double>(left_odom_dist / theta)) + left_offset.first;
-			local_dist_x = 2 * x_radius * (sin(theta / 2));
-			local_dist_y = 2 * y_radius * (sin(theta / 2));
-		} else {
-			local_dist_x = rear_odom_dist;
-			local_dist_y = left_odom_dist;
+		if(heading_change != 0_deg){
+			x_radius = (rear_dist / heading_change) + rear_offset.second;
+			y_radius = (left_dist / heading_change) + left_offset.first;
+			local_x = 2 * x_radius * sin(heading_change / 2);
+			local_y = 2 * y_radius * sin(heading_change / 2);
 		}
+		else{
+			local_x = rear_dist / 1_rad;
+			local_y = left_dist / 1_rad;
+		}
+		
+		std::cout << "x: " << odom_x << " y: " << odom_y << " lx: " << local_x << " ly: " << local_y << 
+		" left: " << left_odom.to<double>() << " rear: " << rear_odom.to<double>() << " theta: " << heading_change
+		<< " IMUH:"<< imu.get_heading() << " GH: " << global_true_heading << " LTH: " << local_true_heading << std::endl;
 
-		std::pair<inch_t, inch_t> global =
-		    local_to_global_coords(local_dist_x, local_dist_y, imu_heading);
-		tracking_x += global.first;
-		tracking_y += global.second;
+		local_true_heading = atan2(local_y, local_x);
+		if(local_x == 0_in && local_y == 0_in) local_true_heading = 0_deg;
+		global_true_heading = local_true_heading + robot_heading;
+		while(global_true_heading > 180_deg) global_true_heading -= 360_deg;
+		while(global_true_heading < 180_deg) global_true_heading += 360_deg;
 
-		std::pair<inch_t, inch_t> offset = local_to_global_coords(
-		    inch_t(left_offset.second), inch_t(rear_offset.first), imu_heading
-		);
-		odom_x = tracking_x + offset.first;
-		odom_y = tracking_y + offset.second;
+		total_dist = sqrt(pow<2>(local_x) + pow<2>(local_y));
+		odom_x += total_dist * cos(global_true_heading);
+		odom_y += total_dist * sin(global_true_heading);
 
 		odom_left.reset_position();
 		odom_rear.reset_position();
-		previous_heading = imu_heading;
+		left_odom = 0_deg;
+		rear_odom = 0_deg;
+		prev_heading = robot_heading;
 
-		pros::delay(10);
+		
+		pros::delay(20);
+
 	}
+	
 }
 
 void odom::calibrate(inch_t robot_x = 0_in, inch_t robot_y = 0_in, degree_t heading = 0_deg) {
