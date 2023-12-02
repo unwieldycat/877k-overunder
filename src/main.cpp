cb3118fbdb6eb6d89134079d68023d30d0d0cb09
Thurston Yates <thurstonalexyates@gmail.com>
2023-12-02 07:48:40 -0500

    Upgrade robodash idk

--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,18 @@
 #include "odom.hpp"
 #include "subsystems/catapult.hpp"
 #include "subsystems/misc.hpp"
+#include <sys/select.h>
 
-rd::SelectorView selector;
-rd::ImageView logo("Logo", "/usd/logo.bin");
+rd::Selector selector;
+rd::Image logo("Logo", "/usd/logo.bin");
 
 // ========================== Initialize Functions ========================== //
 
 void initialize() {
-	rd::initialize();
 	odom::initialize();
 	strip_left.set_all(0xff6600);
 	strip_right.set_all(0xff6600);
 	cata_rot.reset_position();
-
-	// Configure GUI
-	rd::register_views({&selector, &logo});
 
 	selector.add_autons(
 	    {{"Park (left)", park_left},
@@ -34,22 +31,19 @@ void initialize() {
 	pros::Task odom_task(odom::track_position, "Odometry");
 }
 
-void competition_initialize() { rd::set_view(&selector); }
+void competition_initialize() { selector.focus(); }
 
 // ============================ Match Functions ============================ //
 
 void autonomous() {
-	rd::set_view(&logo);
+	logo.focus();
 
 	odom_pistons.extend();
-	pros::delay(1000);
-	odom_pistons.retract();
 
 	selector.do_auton();
 }
 
 void opcontrol() {
-	rd::set_view(&logo);
 	pros::Task drive_task(chassis::user_drive, "User Chassis Control");
 	pros::Task cata_task(cata::user, "Catapult Control");
 	pros::Task misc_task(user, "Misc User Control");
