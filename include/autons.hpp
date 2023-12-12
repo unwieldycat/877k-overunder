a5be70696d7f0fae35525ed72c559151eacbbbc0
Thurston Yates <thurstonalexyates@gmail.com>
2023-12-12 10:33:24 -0500

    Remove useless autons

--- include/autons.hpp
+++ include/autons.hpp
@@ -2,16 +2,6 @@
 #include "main.h"
 
 /**
- * Park robot next to left goal, starting from left side
- */
-void park_left();
-
-/**
- * Park robot next to right goal, starting from right side
- */
-void park_right();
-
-/**
  * Push the field triball into goal
  */
 void push_field();
