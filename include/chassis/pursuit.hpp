7e63a36493a0cc1d56b2c782c0a1967018eeb2fd
Kit <yuhanli2006@gmail.com>
2023-12-14 19:03:47 -0500

    update comments

--- include/chassis/pursuit.hpp
+++ include/chassis/pursuit.hpp
@@ -18,20 +18,13 @@ extern std::vector<Point> points;
  *
  * @param x_ft x coordinate in feet
  * @param y_ft y coordinate in feet
+ * @param curvature curvature |<x'(t), y'(t)> cross <x"(t), y"(t)>|/|<x'(t), y'(t)>|^3
  */
 void add_point(foot_t x_ft, foot_t y_ft, units::dimensionless::scalar_t curvature);
 
 /**
  * @brief Begins moving the robot following all the points stored previous to the running of this
  * function, clears the points after running
- *
- * @param lookahead_Distance How far away the point the robot is "Chasing" is
- * @param voltage_constant How fast the robot will be moving forward if it is traveling on a
- * straight path
- * @param lowest_x Bottom left x coordinate in feet of the restricted area
- * @param lowest_y Bottom left y coordinate in feet of the restricted area
- * @param highest_x Bottom right x coordinate in feet of the restricted area
- * @param highest_y Bottom right y coordinate in feet of the restricted area
  */
 void pursuit();
 
