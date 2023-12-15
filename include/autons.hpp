3e600ffbf9aa09e003518c05a6b14f3a9f169c60
Thurston Yates <thurstonalexyates@gmail.com>
2023-12-15 15:30:51 -0500

    awp lol

--- include/autons.hpp
+++ include/autons.hpp
@@ -16,3 +16,9 @@ void push_preload();
  * Starts on second tile from blue low bar
  */
 void skills();
+
+/**
+ * Does half of the autonomous win point. Scores triball and touches climb bar
+ * Starts on first tile from blue low bar facing midline.
+ */
+void awp();
---
+++ src/autons/awp.cpp
@@ -0,0 +1,9 @@
+#include "autons.hpp"
+#include "chassis/auton.hpp"
+#include "devices.hpp"
+
+void awp() {
+	// TODO: Make it do stuff
+	// Run push auton
+	// Go to climbing bar and tap somehow
+}
