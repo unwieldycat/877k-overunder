3b6c2e580876d81c568fee4a148eaafe98c396c7
Thurston Yates <thurstonalexyates@gmail.com>
2023-12-16 08:49:45 -0500

    working cata code

--- src/subsystems/catapult.cpp
+++ src/subsystems/catapult.cpp
@@ -2,24 +2,27 @@
 #include "devices.hpp"
 #include "pros/motors.h"
 
-const int release_angle = -4000;
+const int release_angle = 7500;
 
-bool cata::is_primed() { return cata_rot.get_position() < release_angle; }
+bool cata::is_primed() { return cata_rot.get_position() >= release_angle; }
 
 void cata::prime() {
-	while (cata_rot.get_position() > release_angle) {
+	while (cata_rot.get_position() < release_angle) {
 		catapult.move(127);
 		pros::delay(10);
 	}
+	catapult.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
 	catapult.brake();
 }
 
 void cata::release() {
-	if (cata_rot.get_position() > release_angle) return; // Make sure cata is primed
+	if (!cata::is_primed()) return; // Make sure cata is primed
 
 	catapult.move(127);
-	while (cata_rot.get_position() < release_angle)
+	while (cata_rot.get_position() >= release_angle)
 		pros::delay(50);
+
+	catapult.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
 	catapult.brake();
 }
 
@@ -28,22 +31,28 @@ void cata::user() {
 	bool brake = false;
 	catapult.set_brake_mode(pros::E_MOTOR_BRAKE_COAST);
 	while (true) {
-		if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_A)) continuous = !continuous;
+		// if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_A)) continuous = !continuous;
 
-		/* FIXME: Catapult never stops
-		if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_B) && !continuous) {
-		    if (!cata::is_primed()) cata::prime();
-		    cata::release();
-		    cata::prime();
-		    pros::delay(20);
-		    return;
-		}*/
+		if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_B)) {
+			std::cout << cata::is_primed() << "\n";
+			if (!cata::is_primed())
+				cata::prime();
+			else {
+				cata::release();
+				pros::delay(250);
+				cata::prime();
+			}
 
-		if (continuous) {
-			catapult.move(96);
-		} else {
-			catapult.brake();
+			pros::delay(20);
+			continue;
 		}
+
+		/*
+		if (continuous) {
+		    catapult.move(104);
+		} else {
+		    catapult.brake();
+		}*/
 
 		pros::delay(20);
 	}
