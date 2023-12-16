95433e14d90f60a342a69aea398cd4b596a21139
Thurston Yates <thurstonalexyates@gmail.com>
2023-12-15 23:10:25 -0500

    Actually try to move to the match loader

--- src/autons/skills.cpp
+++ src/autons/skills.cpp
@@ -8,6 +8,13 @@
 const int launches = 50;
 
 void skills() {
+	chassis::drive(90);
+	pros::delay(750);
+	chassis::drive(0);
+	chassis::turn_rel(45_deg);
+	chassis::drive(-90);
+	pros::delay(500);
+	chassis::drive(-32);
 	catapult.move(96);
 	/*
 	// Drive to match loader
