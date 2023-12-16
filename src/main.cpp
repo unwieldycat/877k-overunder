a495dabe42794bc9ad0d274c3d9f6edf895b21f0
Thurston Yates <thurstonalexyates@gmail.com>
2023-12-16 08:53:03 -0500

    Ensure init runs and  remove stuff from init

--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,12 @@ rd::Image logo("Logo", "/usd/logo.bin");
 
 // ========================== Initialize Functions ========================== //
 
+bool initialized = false;
 void initialize() {
+	if (initialized) return;
+	initialized = true;
+
 	odom::initialize();
-	strip_left.set_all(0xff6600);
-	strip_right.set_all(0xff6600);
 	cata_rot.reset_position();
 
 	// Tasks
@@ -36,14 +38,15 @@ void competition_initialize() {
 // ============================ Match Functions ============================ //
 
 void autonomous() {
+	initialize();
+
 	logo.focus();
-
 	odom_pistons.extend();
-
 	selector.do_auton();
 }
 
 void opcontrol() {
+	initialize();
 	pros::Task drive_task(chassis::user_drive, "User Chassis Control");
 	pros::Task cata_task(cata::user, "Catapult Control");
 	pros::Task misc_task(user, "Misc User Control");
