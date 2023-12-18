9a3780f8417569bf90bc7ace0c43b8f94e5b0ad5
Thurston Yates <thurstonalexyates@gmail.com>
2023-12-18 16:45:39 -0500

    Update robodash

--- src/main.cpp
+++ src/main.cpp
@@ -8,48 +8,34 @@
 #include "subsystems/misc.hpp"
 #include <sys/select.h>
 
-rd::Selector selector;
+rd::Selector selector({
+    {"Field Triball Push", push_field},
+    {"Preload Triball Push", push_preload},
+    {"Skills", skills},
+});
+
 rd::Image logo("Logo", "/usd/logo.bin");
 
 // ========================== Initialize Functions ========================== //
 
-bool initialized = false;
 void initialize() {
-	if (initialized) return;
-	initialized = true;
-
 	odom::initialize();
 	cata_rot.reset_position();
 
-	// Tasks
 	pros::Task odom_task(odom::track_position, "Odometry");
 }
 
-void competition_initialize() {
-	initialize();
-
-	selector.add_autons(
-	    {{"Field Triball Push", push_field},
-	     {"Preload Triball Push", push_preload},
-	     {"Skills", skills}}
-	);
-
-	selector.focus();
-}
+void competition_initialize() { selector.focus(); }
 
 // ============================ Match Functions ============================ //
 
 void autonomous() {
-	initialize();
-
 	logo.focus();
 	odom_pistons.extend();
-	selector.do_auton();
+	selector.run_auton();
 }
 
 void opcontrol() {
-	initialize();
-
 	pros::Task drive_task(chassis::user_drive, "User Chassis Control");
 	pros::Task cata_task(cata::user, "Catapult Control");
 	pros::Task misc_task(user, "Misc User Control");
