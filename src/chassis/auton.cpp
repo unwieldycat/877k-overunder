3b373e575cc3e071db9b4f7c70f3c3cea2aaaef3
Thurston Yates <thurstonalexyates@gmail.com>
2023-12-30 17:23:23 -0500

    Unify PID controllers

--- src/chassis/auton.cpp
+++ src/chassis/auton.cpp
@@ -3,6 +3,9 @@
 
 using namespace units::math;
 
+PIDController<inch_t> drive_pid(2, 0, 0);
+PIDController<degree_t> turn_pid(3, 0.1, 1);
+
 // ============================ Auton Functions ============================ //
 
 void chassis::drive(int power) {
@@ -11,9 +14,6 @@ void chassis::drive(int power) {
 }
 
 void chassis::drive(foot_t distance) {
-	PIDController<inch_t> drive_pid(2, 0, 0);
-	PIDController<degree_t> turn_pid(1, 0, 0);
-
 	inch_t origin_x = odom::get_x();
 	inch_t origin_y = odom::get_y();
 	inch_t traveled;
@@ -21,6 +21,9 @@ void chassis::drive(foot_t distance) {
 	double turn;
 	degree_t target_rot = (degree_t)imu.get_rotation();
 	degree_t current_rot;
+
+	drive_pid.reset();
+	turn_pid.reset();
 
 	do {
 		traveled = sqrt(pow<2>(odom::get_x() - origin_x) + pow<2>(odom::get_y() - origin_y));
@@ -37,39 +40,37 @@ void chassis::drive(foot_t distance) {
 }
 
 void chassis::turn_abs(degree_t heading) {
-	PIDController<degree_t> pid(3, 0.1, 1);
-
 	double output;
 	degree_t current_hdg;
 
 	heading += (degree_t)360.0 * ((int)imu.get_rotation() / 360);
+	turn_pid.reset();
 
 	do {
 		current_hdg = degree_t(imu.get_rotation());
-		output = pid.calculate(heading, current_hdg);
+		output = turn_pid.calculate(heading, current_hdg);
 		drive_left.move(output);
 		drive_right.move(-output);
 		pros::delay(20);
-	} while (!pid.settled());
+	} while (!turn_pid.settled());
 
 	drive_left.brake();
 	drive_right.brake();
 }
 
 void chassis::turn_rel(degree_t degrees) {
-	PIDController<degree_t> pid(3, 0.1, 1);
-
 	double output;
 	degree_t current_rot;
 	degree_t end_rot = (degree_t)imu.get_rotation() + degrees;
+	turn_pid.reset();
 
 	do {
 		current_rot = degree_t(imu.get_rotation());
-		output = pid.calculate(end_rot, current_rot);
+		output = turn_pid.calculate(end_rot, current_rot);
 		drive_left.move(output);
 		drive_right.move(-output);
 		pros::delay(20);
-	} while (!pid.settled());
+	} while (!turn_pid.settled());
 
 	drive_left.brake();
 	drive_right.brake();
