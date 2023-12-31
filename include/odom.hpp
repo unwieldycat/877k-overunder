940419d20af3787ca594a6ed78a649f793bdc564
Thurston Yates <thurstonalexyates@gmail.com>
2023-12-31 12:51:06 -0500

    Remove undefined functions

--- include/odom.hpp
+++ include/odom.hpp
@@ -9,37 +9,13 @@ namespace odom {
 void initialize();
 
 /**
- * @brief Convert local coordinates to global coordinates
- * 
- * @param local_x Local coordinate X
- * @param local_y Local coordinate Y
- * @param robot_heading Current heading
- * @return Global coordinate pair
- */
-std::pair<inch_t, inch_t> local_to_global_coords(inch_t local_x, inch_t local_y, degree_t robot_heading);
-
-/**
- * @brief Convert global coordinates to local coordinates
- * 
- * @param global_x Global coordinate X
- * @param global_y Global coordinate Y
- * @param robot_x Robot X position
- * @param robot_y Robot Y position
- * @param robot_heading Current heading
- * @return Local coordinate pair
- */
-std::pair<inch_t, inch_t> global_to_local_coords(
-    inch_t global_x, inch_t global_y, inch_t robot_x, inch_t robot_y, degree_t robot_heading
-);
-
-/**
  * @brief Position tracking function
  */
 [[noreturn]] void track_position();
 
 /**
  * @brief Calibrate odometry
- * 
+ *
  * @param robot_x Start position x
  * @param robot_y Start position y
  * @param heading Starting heading
@@ -48,15 +24,15 @@ void calibrate(inch_t robot_x, inch_t robot_y, degree_t heading);
 
 /**
  * @brief Get the current x value
- * 
- * @return double 
+ *
+ * @return double
  */
 inch_t get_x();
 
 /**
  * @brief Get the current y value
- * 
- * @return double 
+ *
+ * @return double
  */
 inch_t get_y();
 
