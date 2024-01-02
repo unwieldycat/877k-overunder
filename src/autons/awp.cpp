1f7216fe0704de3bc2a387ad5e0961c095d9d8e7
Kit <yuhanli2006@gmail.com>
2024-01-01 21:01:13 -0500

    add paths to awp auton routine

--- src/autons/awp.cpp
+++ src/autons/awp.cpp
@@ -4,4 +4,6 @@ void awp() {
 	// TODO: Make it do stuff
 	// Run push auton
 	// Go to climbing bar and tap somehow
+	chassis::pursuit("/usd/paths/awp_auton0.csv");
+	chassis::pursuit("/usd/paths/awp_auton1p.csv", true);
 }
