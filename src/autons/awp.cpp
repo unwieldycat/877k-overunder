f98454274ece9fc60644151ffd115468e033cdc0
Kit <yuhanli2006@gmail.com>
2024-01-01 21:02:16 -0500

    Add starting position

--- src/autons/awp.cpp
+++ src/autons/awp.cpp
@@ -4,6 +4,7 @@ void awp() {
 	// TODO: Make it do stuff
 	// Run push auton
 	// Go to climbing bar and tap somehow
+	odom::calibrate(11_ft, 3_ft, 180_deg);
 	chassis::pursuit("/usd/paths/awp_auton0.csv");
 	chassis::pursuit("/usd/paths/awp_auton1p.csv", true);
 }
