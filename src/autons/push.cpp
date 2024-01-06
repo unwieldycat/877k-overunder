777f7c050e77d5b0dac2841ec7a3a9940f89ecf3
Thurston Yates <thurstonalexyates@gmail.com>
2024-01-06 09:26:56 -0500

    Fix direction

--- src/autons/push.cpp
+++ src/autons/push.cpp
@@ -1,7 +1,7 @@
 #include "main.h"
 
 void push_left() {
-	odom::calibrate(0_ft, 0_ft, 45_deg);
+	odom::calibrate(0_ft, 0_ft, -45_deg);
 	chassis::drive(2.2_ft);
 	chassis::drive(127, 0_deg, 2_s);
 	chassis::drive(-127, 0_deg, 1_s);
@@ -9,7 +9,7 @@ void push_left() {
 }
 
 void push_right() {
-	odom::calibrate(0_ft, 0_ft, -45_deg);
+	odom::calibrate(0_ft, 0_ft, 45_deg);
 	chassis::drive(2.2_ft);
 	chassis::drive(127, 0_deg, 2_s);
 	chassis::drive(-127, 0_deg, 1_s);
