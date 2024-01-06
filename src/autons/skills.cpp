223a788eb405c662c625745a75e82709e83c561f
Thurston Yates <thurstonalexyates@gmail.com>
2024-01-06 12:55:21 -0500

    Slow down puncher

--- src/autons/skills.cpp
+++ src/autons/skills.cpp
@@ -7,7 +7,7 @@ void skills() {
 	// Set odom to starting point
 	odom::calibrate(2_ft, 2_ft, 225_deg);
 
-	punch_motors.move(127);
+	punch_motors.move(104);
 
 	// chassis::pursuit("/usd/paths/skills0.csv", true);
 	// chassis::pursuit("/usd/paths/skills1.csv");
