757d1738fcf8a631489b1d70c0d753ed0c3af24a
Thurston Yates <thurstonalexyates@gmail.com>
2024-01-06 08:24:18 -0500

    Adjust route

--- src/autons/awp.cpp
+++ src/autons/awp.cpp
@@ -13,16 +13,18 @@ void awp_right() {
 	chassis::drive(2.5_ft);
 	right_wing.extend();
 	chassis::turn_abs(-45_deg);
-	chassis::drive(2_ft);
+	chassis::drive(1.414_ft);
 	left_wing.extend();
-	chassis::drive(12_in, 90_deg);
+	chassis::turn_abs(0_deg);
+	chassis::drive(1.5_ft, 0_deg);
+	chassis::drive(1_ft, 90_deg);
 	chassis::drive(127, 90_deg, 1_s);
-	chassis::drive(-127, 90_deg, 1_s);
-	chassis::drive(3_in, 90_deg);
+	chassis::drive(-127, 90_deg, 2_s);
+	chassis::drive(6_in, 90_deg);
 	right_wing.retract();
 	left_wing.retract();
 	chassis::turn_abs(180_deg);
 	chassis::drive(2.5_ft, 180_deg);
 	right_wing.extend();
-	chassis::turn_rel(-15_deg);
+	chassis::turn_rel(-45_deg);
 }
