da0a579f6e9be9505c8fb20ac22b9b5580f88efa
Kit <yuhanli2006@gmail.com>
2024-01-13 23:08:18 -0500

    Organize Point Class

--- src/point.cpp
+++ src/point.cpp
@@ -1,8 +1,20 @@
+#include "point.hpp"
 #include "main.h"
 #include <utility>
 
 using namespace units::math;
 
+// All Constructors
+Point::Point(foot_t x, foot_t y) : Point(x, y, 0, false, false) {}
+Point::Point(foot_t x, foot_t y, units::angle::degree_t heading) : x(x), y(y), heading(heading) {}
+Point::Point(foot_t x, foot_t y, units::dimensionless::scalar_t curvature)
+    : Point(x, y, curvature, false, false) {}
+Point::Point(
+    foot_t x, foot_t y, units::dimensionless::scalar_t curvature, bool left_wing, bool right_wing
+)
+    : x(x), y(y), curvature(curvature), left_wing(left_wing), right_wing(right_wing) {}
+
+// Calculation Methods
 units::dimensionless::scalar_t Point::calc_par_slope(Point a, Point b) {
 	return (b.y - a.y) / (b.x - a.x);
 }
@@ -18,4 +30,11 @@ foot_t Point::calc_dist(Point a, Point b) { return sqrt(pow<2>(b.x - a.x) + pow<
 
 std::pair<foot_t, foot_t> Point::xy_dist(Point a, Point b) {
 	return std::make_pair(b.x - a.x, b.y - a.y);
+}
+
+// Mutator Functions
+void Point::update(foot_t tx, foot_t ty, units::angle::degree_t theading) {
+	x = tx;
+	y = ty;
+	heading = theading;
 }
