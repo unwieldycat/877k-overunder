608304c46bd8044a2e88e68839478f9dec59cbd9
Thurston Yates <thurstonalexyates@gmail.com>
2024-01-22 08:21:43 -0500

    Clean up push routines

--- src/autons/push.cpp
+++ src/autons/push.cpp
@@ -1,19 +1,27 @@
 #include "main.h"
 
 void push_left() {
-	odom::calibrate(0_ft, 0_ft, -45_deg);
-	chassis::drive(2.2_ft);
-	chassis::drive(127, 0_deg, 2_s);
-	chassis::drive(-127, 0_deg, 1_s);
+	odom::calibrate(2_ft, 11_ft, -45_deg);
+
+	// Drive to side of goal
+	chassis::drive(2.82_ft, -45_deg);
+	chassis::turn_abs(0_deg);
+
+	// Ram triball into goal
 	chassis::drive(127, 0_deg, 1_s);
-	chassis::drive(-127, 0_deg, 3_s);
+	chassis::drive(-104, 0_deg, 1_s);
+	chassis::drive(127, 0_deg, 1_s);
 }
 
 void push_right() {
-	odom::calibrate(0_ft, 0_ft, 45_deg);
-	chassis::drive(2.2_ft);
-	chassis::drive(127, 0_deg, 2_s);
-	chassis::drive(-127, 0_deg, 1_s);
+	odom::calibrate(9_ft, 11_ft, 45_deg);
+
+	// Drive to side of goal
+	chassis::drive(2.82_ft, 45_deg);
+	chassis::turn_abs(0_deg);
+
+	// Ram triball into goal
 	chassis::drive(127, 0_deg, 1_s);
-	chassis::drive(-127, 0_deg, 3_s);
+	chassis::drive(-104, 0_deg, 1_s);
+	chassis::drive(127, 0_deg, 1_s);
 }
