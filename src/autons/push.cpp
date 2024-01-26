17e8bc26c8f3756478fcea0048c0f0c167fb17ad
Thurston Yates <thurstonalexyates@gmail.com>
2024-01-26 07:57:33 -0500

    Adjust push auton distances

--- src/autons/push.cpp
+++ src/autons/push.cpp
@@ -4,24 +4,22 @@ void push_left() {
 	odom::calibrate(2_ft, 11_ft, -45_deg);
 
 	// Drive to side of goal
-	chassis::drive(2.82_ft, -45_deg);
+	chassis::drive(2.4_ft, -45_deg);
 	chassis::turn_abs(0_deg);
 
 	// Ram triball into goal
 	chassis::drive(127, 0_deg, 1_s);
-	chassis::drive(-104, 0_deg, 1_s);
-	chassis::drive(127, 0_deg, 1_s);
+	chassis::drive(-104, 0_deg, 500_ms);
 }
 
 void push_right() {
 	odom::calibrate(9_ft, 11_ft, 45_deg);
 
 	// Drive to side of goal
-	chassis::drive(2.82_ft, 45_deg);
+	chassis::drive(2.4_ft, 45_deg);
 	chassis::turn_abs(0_deg);
 
 	// Ram triball into goal
 	chassis::drive(127, 0_deg, 1_s);
-	chassis::drive(-104, 0_deg, 1_s);
-	chassis::drive(127, 0_deg, 1_s);
+	chassis::drive(-104, 0_deg, 500_ms);
 }
