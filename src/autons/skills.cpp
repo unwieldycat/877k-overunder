4611624421d0c1821e587a18e7879e7769444965
Thurston Yates <thurstonalexyates@gmail.com>
2024-02-02 17:26:41 -0500

    temp skills

--- src/autons/skills.cpp
+++ src/autons/skills.cpp
@@ -20,28 +20,30 @@ void skills() {
 	drive_right.brake();
 	odom::calibrate(2_ft, 11.5_ft, -135_deg);
 
+	chassis::drive(-64, 250_ms);
+
 	// Align robot to cross field
 	chassis::turn_rel(-135_deg);
 	chassis::turn_rel(30_deg);
-	chassis::drive(7_in);
-	chassis::turn_abs(90_deg);
-	chassis::drive(3_in);
+	chassis::drive(9_in);
+	chassis::turn_rel(-30_deg);
 
 	// Drive to other side of field
-	chassis::drive(6_ft, 90_deg);
-	chassis::turn_abs(0_deg);
-	chassis::drive(2_ft, 0_deg);
-	chassis::turn_abs(-45_deg);
-	chassis::drive(1.41_ft, -45_deg);
+	chassis::drive(4_ft);
+	chassis::drive(1.4_ft);
+	chassis::turn_rel(-90_deg);
+
+	chassis::drive(2_ft);
+	chassis::turn_rel(-45_deg);
+	chassis::drive(1.41_ft);
 
 	// Push triballs
 	left_wing.extend();
 	right_wing.extend();
-	pros::delay(250);
 
-	chassis::turn_abs(0_deg);
+	chassis::turn_rel(45_deg);
 	chassis::drive(96, 0_deg, 1_s);
-	chassis::turn_abs(90_deg);
+	chassis::turn_rel(90_deg);
 
 	// Push into goal
 	chassis::drive(96, 90_deg, 1_s);
