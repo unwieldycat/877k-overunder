ee0fa2b6dad517d1e4055efa6384e8f75cb3ab76
Thurston Yates <thurstonalexyates@gmail.com>
2024-02-03 21:33:45 -0500

    New push right auton
    Simplify old auton and comment out, and create new one

--- src/autons/push.cpp
+++ src/autons/push.cpp
@@ -32,36 +32,23 @@ void push_left() {
 }
 
 void push_right() {
+	odom::calibrate(0_ft, 0_ft, 45_deg);
+	chassis::drive(2.8_ft, 45_deg);
+	chassis::turn_abs(0_deg);
+	chassis::drive(127, 1_s);
+	chassis::drive(-127, 750_ms);
+	chassis::drive(127, 1_s);
+	chassis::drive(-127, 750_ms);
+
+	/* Temp auton
 	chassis::d_drive(20, 50);
 	pros::delay(300);
 	chassis::d_turn(70.0, 50);
 	pros::delay(300);
 	chassis::d_drive(2, 50);
 
-	pros::delay(1000);
-
-	// Push
 	chassis::d_drive(-4, 50);
-	pros::delay(300);
 	chassis::d_turn(-90);
-	pros::delay(300);
 	chassis::drive(127, 1.1_s);
-	pros::delay(300);
-
-	// Cross
-	chassis::drive(-127, 750_ms);
-	pros::delay(300);
-
-	drive_right.move(-32);
-	drive_left.move(-127);
-
-	pros::delay(3000);
-
-	drive_left.brake();
-	drive_right.brake();
-
-	chassis::drive(2.3_ft);
-	chassis::turn_rel(-45_deg);
-	chassis::drive(2_ft);
-	left_wing.extend();
+	chassis::drive(-127, 750_ms);*/
 }
