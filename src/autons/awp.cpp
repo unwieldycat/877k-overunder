37a4ef49d7d7be1a2f4357032615bfce882ca4dd
Thurston Yates <thurstonalexyates@gmail.com>
2024-02-03 21:19:57 -0500

    AWP left changes

--- src/autons/awp.cpp
+++ src/autons/awp.cpp
@@ -1,19 +1,27 @@
 #include "main.h"
 
-// TODO: Experiment with going forwards to clear triball
 void awp_left() {
-	odom::calibrate(11.25_ft, 1.5_ft, -45_deg);
+	odom::calibrate(11.25_ft, 1.5_ft, 90_deg);
+
+	chassis::drive(2_ft);
+	chassis::drive(-(2_ft + 3_in));
+	chassis::turn_abs(-45_deg);
 
 	// Remove triball from matchload zone
 	chassis::drive(1_ft);
 	left_wing.extend();
 	chassis::drive(-1_ft);
-	chassis::turn_rel(-270_deg);
+	chassis::turn_rel(-90_deg);
+	chassis::drive(-64, 750_ms);
+	left_wing.retract();
+
+	// Realign to go to climber
+	chassis::drive(64, 1.25_s);
+	odom::calibrate(0_ft, 0_ft, -135_deg);
 
 	// Drive to touch climbing bar
-	left_wing.retract();
-	chassis::drive(2.28_ft);
-	chassis::turn_abs(90_deg); // FIXME: This turns 90 degrees, not to 90 degrees??
+	chassis::drive(-2.28_ft);
+	chassis::turn_abs(90_deg);
 	chassis::drive(2_ft);
 	right_wing.extend();
 	chassis::turn_rel(-15_deg);
