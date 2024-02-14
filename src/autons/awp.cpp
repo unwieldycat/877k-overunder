c40451639593bc07500bbd1baaeb285e91c84b49
Thurston Yates <thurstonalexyates@gmail.com>
2024-02-14 09:26:52 -0500

    New AWP

--- src/autons/awp.cpp
+++ src/autons/awp.cpp
@@ -4,25 +4,23 @@ void awp() {
 	odom::calibrate(11.25_ft, 1.5_ft, 90_deg);
 
 	chassis::drive(2_ft);
-	chassis::drive(-(2_ft + 3_in));
-	chassis::turn_abs(-45_deg);
+	chassis::turn_abs(-135_deg);
+	chassis::drive(2_ft + 3_in);
 
 	// Remove triball from matchload zone
-	chassis::drive(1_ft);
+	chassis::turn_rel(30_deg);
 	left_wing.extend();
-	chassis::drive(-1_ft);
-	chassis::turn_rel(-90_deg);
-	chassis::drive(-64, 750_ms);
+	chassis::turn_abs(90_deg, chassis::TurnSide::Left);
 	left_wing.retract();
 
+	// TODO: Make triball removal consistent and find position robot ends
+
 	// Realign to go to climber
-	chassis::drive(64, 1.25_s);
-	odom::calibrate(0_ft, 0_ft, -135_deg);
+	chassis::drive(-2_in);
+	chassis::turn_abs(90_deg);
 
 	// Drive to touch climbing bar
-	chassis::drive(-2.28_ft);
-	chassis::turn_abs(90_deg);
-	chassis::drive(2_ft);
+	chassis::drive(4_ft, 90_deg);
 	right_wing.extend();
 	chassis::turn_rel(-15_deg);
 }
