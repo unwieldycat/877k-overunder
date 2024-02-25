448d6dc7e4d38b1111eb098a9fbf4b81f6b65cee
Kit <yuhanli2006@gmail.com>
2024-02-24 22:27:58 -0500

    "Working" Skills

--- src/autons/skills.cpp
+++ src/autons/skills.cpp
@@ -1,21 +1,20 @@
+#include "chassis/auton.hpp"
+#include "devices.hpp"
 #include "main.h"
+#include <iostream>
 
 const int launch_time = 45000;
 
 void skills() {
-	odom::calibrate(0_ft, 0_ft, -45_deg);
+	odom::calibrate(0_ft, 0_ft, 180_deg);
 
-	// Push preload in
-	chassis::drive(2_ft);
-	chassis::turn_abs(0_deg, chassis::TurnSide::Left);
-	chassis::drive(127, 1_s);
-	chassis::drive(-1_ft, 0_deg);
-
-	// Align to launch
-	chassis::turn_abs(180_deg);
-	chassis::drive(8_in);
-	chassis::turn_abs(-90_deg - 15_deg);
+	// align to launch
+	chassis::drive(23_in);
+	chassis::turn_rel(-95_deg);
+	chassis::drive(20_in);
 	left_wing.extend();
+
+	// LAUNCH!
 
 	punch_motors.move(104);
 
@@ -25,7 +24,34 @@ void skills() {
 
 	punch_motors.brake();
 
-	// TODO: push into goal
+	// Cross
+	pros::delay(500);
+	left_wing.retract();
+	chassis::drive(-16_in);
+	chassis::turn_rel(-145_deg, chassis::TurnSide::Right);
+	chassis::drive(15_in);
+	chassis::turn_rel(-25_deg, chassis::TurnSide::Right);
+	chassis::drive(5.4_ft, 275_deg);
+
+	// Push into goal
+	chassis::turn_rel(-37_deg, chassis::TurnSide::Right);
+	chassis::drive(18_in);
+	chassis::turn_rel(-40_deg);
+	chassis::drive(127, 1.5_s);
+	chassis::drive(-8_in);
+	chassis::turn_rel(-80_deg);
+	chassis::drive(40_in);
+	chassis::turn_rel(90_deg);
+	left_wing.extend();
+	right_wing.extend();
+	chassis::drive(2_ft);
+	chassis::turn_rel(45_deg, chassis::TurnSide::Left);
+	chassis::drive(127, 4_s);
+	chassis::drive(-127, 1.5_s);
+	chassis::turn_rel(-90_deg);
+	chassis::drive(1.5_ft);
+	chassis::turn_rel(90_deg);
+	chassis::drive(127, 2_s);
 
 	// chassis::pursuit("/usd/paths/skills0.csv", true);
 	// chassis::pursuit("/usd/paths/skills1.csv");
