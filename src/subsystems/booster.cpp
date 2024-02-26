ff0d3a6cb50b9c3b527fdfd672ee4d9d85213ce8
Thurston Yates <thurstonalexyates@gmail.com>
2024-02-26 07:23:46 -0500

    Manual boosters

--- src/subsystems/booster.cpp
+++ src/subsystems/booster.cpp
@@ -8,20 +8,32 @@ void booster::automatic() {
 	int start_pitch = 0;
 
 	while (true) {
+		/*
 		double pitch = imu.get_roll();
 		int pitch_time = pros::millis() - start_pitch;
 
 		if (pitch > boost_pitch && extended && pitch_time > boost_time) {
-			left_lift.retract();
-			right_lift.retract();
-			extended = false;
+		    left_lift.retract();
+		    right_lift.retract();
+		    extended = false;
 		}
 
 		if (pitch < boost_pitch && !extended) {
-			left_lift.extend();
-			right_lift.extend();
-			extended = true;
-			start_pitch = pros::millis();
+		    left_lift.extend();
+		    right_lift.extend();
+		    extended = true;
+		    start_pitch = pros::millis();
+		}
+		*/
+
+		if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_RIGHT)) {
+			if (left_lift.is_extended() || right_lift.is_extended()) {
+				left_lift.retract();
+				right_lift.retract();
+			} else {
+				left_lift.extend();
+				right_lift.extend();
+			}
 		}
 
 		pros::delay(100);
