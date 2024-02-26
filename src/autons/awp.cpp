d239e22715fb165e301d69126f029abf96cafcd2
Thurston Yates <thurstonalexyates@gmail.com>
2024-02-26 10:29:51 -0500

    AWP tuning

--- src/autons/awp.cpp
+++ src/autons/awp.cpp
@@ -5,20 +5,22 @@ void awp() {
 
 	// FIXME: wing cannot remove triball
 	// Remove triball
-	chassis::drive(6_in);
+	// chassis::drive(1_ft);
 	left_wing.extend();
-	chassis::drive(-6_in);
-	chassis::turn_rel(-45_deg, chassis::TurnSide::Right);
+	// chassis::drive(-6_in);
+	chassis::turn_rel(-37_deg, chassis::TurnSide::Right);
+	pros::delay(2000);
 
 	// FIXME: Tune distances to align properly
 	// Drive to touch climbing bar
 	chassis::turn_abs(-135_deg);
 	left_wing.retract();
-	chassis::drive(-2_ft);
+	chassis::drive(-1.9_ft);
 	chassis::turn_abs(90_deg);
-	chassis::drive(2_ft, 90_deg);
+	chassis::drive(1.8_ft, 92_deg);
 
 	// Touch climbing bar
 	right_wing.extend();
+	pros::delay(1000);
 	chassis::turn_rel(-15_deg);
 }
